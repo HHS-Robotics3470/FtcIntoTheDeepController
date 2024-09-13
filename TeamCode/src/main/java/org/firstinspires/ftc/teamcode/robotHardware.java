8b5d3ae4ba007251b374bb88bc828f009856f922
areeb-syed <areeb.syed0307@gmail.com>
2024-09-13 00:24:13 -0700

    Simple robot hardware and teleOP classes created

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotHardware.java
@@ -1,6 +1,85 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
 public class robotHardware {
-    public robotHardware(TeleOpPractice teleOpPractice) {
+    private LinearOpMode myOpMode = null;
+    public robotHardware(LinearOpMode opMode){
+        myOpMode = opMode;
     }
+
+    public double driveSpeedControl = 0.8;
+
+    //Moters
+    public DcMotor fLeft;
+    public DcMotor fRight;
+    public DcMotor bLeft;
+    public DcMotor bRight;
+
+    public void init(){
+
+//        MOTORS
+        fLeft = myOpMode.hardwareMap.get(DcMotor.class, "fLeft");
+        fRight = myOpMode.hardwareMap.get(DcMotor.class, "fRight");
+        bLeft = myOpMode.hardwareMap.get(DcMotor.class, "bLeft");
+        bRight = myOpMode.hardwareMap.get(DcMotor.class, "bRight");
+
+
+        //Direction and encoders
+        fLeft.setDirection(DcMotor.Direction.FORWARD);
+        fRight.setDirection(DcMotor.Direction.REVERSE);
+        bLeft.setDirection(DcMotor.Direction.FORWARD);
+        bRight.setDirection(DcMotor.Direction.REVERSE);
+
+        fLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        fLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        myOpMode.telemetry.addData( "status", "initialized");
+        myOpMode.telemetry.update();
+        myOpMode.waitForStart();
+
+        //Servo initial positions
+
+
+    }
+
+    //Mechnam code for robot
+    public void driveRobot(Gamepad gamepad1){
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = -gamepad1.right_stick_x;
+
+        double frLeft = y + x + rx;
+        double frRight = y - x - rx;
+        double baLeft = y - x + rx;
+        double baRight = y + x - rx;
+
+        setDrivePower(frLeft, frRight, baLeft, baRight);
+
+    }
+
+    //Sets driving speed
+    public void setDrivePower(double v1, double v2, double v3, double v4) {
+
+        fLeft.setPower(v1 * driveSpeedControl);
+        fRight.setPower(v2 * driveSpeedControl);
+        bLeft.setPower(v3 * driveSpeedControl);
+        bRight.setPower(v4 * driveSpeedControl);
+    }
+
+
+
 }
