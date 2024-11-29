edd70e328988eccc3153fbaa8361b09a36f711d0
tinypapercrane <ThePatriots3470FTC@gmail.com>
2024-11-29 12:07:34 -0800

    more roadrunner tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -22,7 +22,7 @@ public class DriveConstants {
      * These are motor constants that should be listed online for your motors.
      */
     public static final double TICKS_PER_REV = 2000;
-    public static final double MAX_RPM = 1;
+    public static final double MAX_RPM = 435;
 
     /*
      * Set RUN_USING_ENCODER to true to enable built-in hub velocity control using drive encoders.
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0143;
-    public static double kA = 0.007;
-    public static double kStatic = 0.01;
+    public static double kV = 0.0145;
+    public static double kA = 0.005;
+    public static double kStatic = 0.0022;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,11 +31,11 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 1; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 9.75; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 9.6; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = 11; // in; offset of the lateral wheel
 
     public static double X_MULTIPLIER = 0.96626; // Multiplier in the X direction
-    public static double Y_MULTIPLIER = 0.95509; // Multiplier in the Y direction
+    public static double Y_MULTIPLIER = 0.95509 * 0.97779429; // Multiplier in the Y direction
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
 
