60afc0116dfb43c62bc16e8a4c69ec9edc8e30d0
Suyash-Dash <158665490+Suyash-Dash@users.noreply.github.com>
2025-01-10 20:34:28 -0800

    NEW AUTOS!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -65,8 +65,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 60;
-    public static double MAX_ACCEL = 60;
+    public static double MAX_VEL = 80;
+    public static double MAX_ACCEL = 70;
     public static double MAX_ANG_VEL = 30;
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
